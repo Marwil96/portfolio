127b1154fb811993f224273a4b25e46020d42895
William_MAC <william_martinsson@hotmail.com>
2017-10-13 23:54:05 +0200

    Add: Sidemenu

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,6 +4,7 @@ import './Components.css';
 import TextLoop from 'react-text-loop';
 import arrowImage from './download.svg';
 import menuButton from './menu.svg';
+import { slide as Menu } from 'react-burger-menu';
 
 var pageOrientation = 0;
 
@@ -44,12 +45,33 @@ class LandingPage extends Component {
       
     }
   }
+  showSettings (event) {
+    event.preventDefault();
+  }
 
   render() {
     return (
       <div className="App">
-      <div className={this.animateMenu()}> </div>
+      
         <div className={this.animateDown()}>
+        <div id="outer-container">
+        <Menu 
+
+          pageWrapId={ "page-wrap" } 
+          outerContainerId={ "outer-container" }
+          right 
+          width={ '18%' } 
+          customBurgerIcon={ <img className="menuButton" src={menuButton}/> 
+          
+          }>
+            <h1 id="menuNamn"> William Martinsson </h1>
+            <a id="home" className="menu-item" href="/">Home</a>
+            <a id="about" className="menu-item" href="/about">About</a>
+            <a id="contact" className="menu-item" href="/contact">Contact</a>
+            <a id="Behance" className="menu-item" href="/contact">Behance</a>
+            <a id="Medium" className="menu-item" href="/contact">Medium</a>
+      </Menu>
+      </div>
           <div className="rubrikContainer"> 
             <h1> <span className='bigHello'>Hello</span><span className='rubrikText'> <br/> <span className='lowOpacity'>I am a</span> <span className='digitalDesigner'>
             <TextLoop children={["Digital Designer", "Frontend Dev", "Learner", "Arsenal Fan"]} speed={1500} adjustingSpeed={200}/> </span> <br/>
@@ -58,10 +80,10 @@ class LandingPage extends Component {
           <div className="arrowContainer">
             <img className="downArrow bounce" src={arrowImage} onClick={this.animate.bind(this)}/>
           </div>
-          <div className="menuContainer">
-            <img className="menuButton" src={menuButton} onClick={this.animateMeny.bind(this)}/>
-          </div>
+
+          
         </div>
+        <div id="killerOfDivs"> </div>
       </div>
     );
   }
