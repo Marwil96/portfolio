ccd3d97980b0ba43a456787aef1a82bc2db1dc9d
William_MAC <william_martinsson@hotmail.com>
2017-10-19 14:25:17 +0200

    Add: Footer, Projectpage

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,8 +5,17 @@ import TextLoop from 'react-text-loop';
 import arrowImage from './download.svg';
 import menuButton from './menu.svg';
 import { slide as Menu } from 'react-burger-menu';
+import Slider from 'react-slick';
+import Scroll from 'react-scroll';
+import {scroller, Element, Link} from 'react-scroll';
 
 var pageOrientation = 0;
+var settings = {
+      dots: false,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 4
+    };
 
 class LandingPage extends Component {
   constructor(props) {
@@ -49,6 +58,7 @@ class LandingPage extends Component {
     event.preventDefault();
   }
 
+
   render() {
     return (
       <div className={this.animateDown()}> 
@@ -71,15 +81,59 @@ class LandingPage extends Component {
             <TextLoop children={["Digital Designer", "Frontend Dev", "Learner", "Arsenal Fan"]} speed={1500} adjustingSpeed={200}/> </span> <br/>
             <span className='lowOpacity'>based in Gothenburg</span></span> </h1>
           </div>
-          <div className="arrowContainer">
+          <Link activeClass="active" className="arrowContainer" to="test1" spy={true} smooth={true} duration={500} >
             <img className="downArrow bounce" src={arrowImage} onClick={this.animate.bind(this)}/>
-          </div>
+          </Link>
 
           
         </div>
-        <div className="projectPageContainer"> 
+        <Element name="test1" className="projectPageContainer">
           <h1 className="projectRubrik"> Project </h1>
-        </div>
+          <h1 className="projectSubheaderText"><span className="projectSubheader"> On The Way </span></h1>
+          <Slider {...settings} className="slider">
+            <div className="sliderContainer project1">
+              <h1 className="projectBoxRubrik">T H R I V E</h1>
+              <h2 className="projectBoxUnderRubrik">Education</h2>
+              <h3 className="projectBoxText">Make a campain site</h3>
+            </div>
+            <div className="sliderContainer project2">
+              <h1 className="projectBoxRubrik">Reporter</h1>
+              <h2 className="projectBoxUnderRubrik">Errors</h2>
+              <h3 className="projectBoxText">An Service built on<br/> react and react native</h3>
+            </div>
+            <div className="sliderContainer project3">
+              <h1 className="projectBoxRubrik">ReactNative</h1>
+              <h2 className="projectBoxUnderRubrik">Learn to write</h2>
+              <h3 className="projectBoxText">A text about the<br/> future of Apps</h3>
+            </div>
+          </Slider>
+          <h1 className="projectSubheaderText subheaderPosition2"><span className="projectSubheader projectSubheaderColor2"> Done </span></h1>
+
+          <Slider {...settings} className="slider">
+            <div className="sliderContainer projectx1">
+              <h1 className="projectBoxRubrik">T H R I V E</h1>
+              <h2 className="projectBoxUnderRubrik">Education</h2>
+              <h3 className="projectBoxText">Make a campain site</h3>
+            </div>
+            <div className="sliderContainer projectx2">
+              <h1 className="projectBoxRubrik">Reporter</h1>
+              <h2 className="projectBoxUnderRubrik">Errors</h2>
+              <h3 className="projectBoxText">An Service built on<br/> react and react native</h3>
+            </div>
+          </Slider>
+          <div className="footerContainer"> 
+            <div className="footerLinkContainer">
+              <h1 className="footerLinkText"> 
+                About<br/>
+                Contact </h1>
+            </div>
+            <div className="footerContactContainer">
+              <h2 className="footerContactText"> William Martinsson
+              <br/> 0768023804
+              <br/> William_martinsson@hotmail.com </h2>
+            </div>
+          </div>
+        </Element>
       </div>
     );
   }
